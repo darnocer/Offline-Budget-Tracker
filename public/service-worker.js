f9f640f5da99c7cca96eafebe08bb4bb19cc37c1
darnocer <darnocer@gmail.com>
2020-07-29 21:52:26 -0600

    files to cache

---
+++ public/service-worker.js
@@ -0,0 +1,4 @@
+const FILES_TO_CACHE = ["/", "/index.html", "/index.js", "/icons/icon-512x512.png", "/icons/icon-192x192.png", "/db.js", "/manifest.json", "/style.css", " "]
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
