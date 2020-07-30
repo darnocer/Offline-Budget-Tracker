610b19afc3c2bd55f0952f405e513c94159f7a0f
darnocer <darnocer@gmail.com>
2020-07-30 16:46:09 -0600

    service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,3 +2,48 @@ const FILES_TO_CACHE = ["/", "/index.html", "/index.js", "/icons/icon-512x512.pn
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+
+// registers service worker
+self.addEventListener('install', function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+
+  
+
+
+  // cache responses for requests for data - opens the data cache
+self.addEventListener('fetch', function(event) {
+    if (event.request.url.includes('/api')) {
+        console.log('[Service Worker] Fetch (data)', event.request.url);
+event.respondWith(
+    caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+        .then(response => {
+            if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+            }
+            return response;
+        })
+        .catch(err => {
+            return cache.match(event.request);
+        });
+    })
+);
+    return;
+}
+event.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+        return cache.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    })
+)
+})
