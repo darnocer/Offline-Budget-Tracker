f46e4a149f890c0f36f8685193d3db4d2055b8bc
darnocer <darnocer@gmail.com>
2020-08-01 16:02:39 -0600

    indexedDB

--- public/db.js
+++ public/db.js
@@ -1,27 +1,74 @@
-// First off we need to create the indexedDB.
-// Then we want to make our db and also open a pane into our indexedDB.
-
+let db;
+// create a new db request for a "budget" database.
+const request = indexedDB.open("budget", 1);
 
 // That opened pane should be set up with functions based on onupgradeneeded, onsuccess, and also onerror.
+request.onupgradeneeded = function (event) {
+  // create object store called "pending" and set autoIncrement to true
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
 
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function (event) {
+  console.log("Woops! " + event.target.errorCode);
+};
 
 // Then we need to use the saveRecord function to save a record into storage of our database.
+function saveRecord(record) {
+  // create a transaction on the pending db with readwrite access
+  const transaction = db.transaction(["pending"], "readwrite");
 
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+
+  // add record to your store with add method.
+  store.add(record);
+}
 
 // We'll also want a function that checks the database (probably called checkDatabase) that will get all of the stored transactions.
-
-
 // onsuccess of this action, we'll use our API bulk post route to push all of the data to our database.
 
+function checkDatabase() {
+  // open a transaction on your pending db
+  const transaction = db.transaction(["pending"], "readwrite");
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+  // get all records from store and set to a variable
+  const getAll = store.getAll();
 
-// If we succeed in our post, then we should clear the store.
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          // if successful, open a transaction on your pending db
+          const transaction = db.transaction(["pending"], "readwrite");
 
+          // access your pending object store
+          const store = transaction.objectStore("pending");
 
+          // clear all items in your store
+          store.clear();
+        });
+    }
+  };
+}
 
-// You should also probably add an event listened that checks if the window has gone back online that also calls that checkDatabase function for good measure.
-
-
-
-// //const transaction = db.transaction(["pending"], "readwrite");
-// const store = transaction.objectStore("pending");
-// store//.[action] where [action] is something like add(record) or clear()
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
